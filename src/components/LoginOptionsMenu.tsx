a6ac6911c1ca9e6de1d90e79948b8c6e4a23b5cc
choiIsabelle <isabellelissina@gmail.com>
2024-01-23 23:34:08 -0500

    close dropdown menu on clickaway

--- src/components/LoginOptionsMenu.tsx
+++ src/components/LoginOptionsMenu.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import  { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import { EyeIcon } from '../Icons/EyeIcon';
 
 const LoginOptionsBtn = styled.h1`
 cursor:pointer;
@@ -12,49 +13,63 @@ margin: 0;
 `
 const DropdownContentContainer = styled.div`
 font-size: 15px;
+margin-top: 0.4rem;
 justify-content: space-between;
 flex-direction: column;
 `
 
 const StyledLink = styled.a`
 color: white;
+cursor:pointer;
 padding-left: 0.2rem
 `
 
-export const LoginOptionsMenu= () => {
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownVisible(!isDropdownVisible);
-  };
-
-  const closeDropdown = (e: React.MouseEvent) => {
-    if (!(e.target as HTMLElement).matches('.dropbtn')) {
-      setDropdownVisible(false);
-    }
-  };
-
-  return (
-    <div>
-      <div className="dropdown">
+const ItemContainer = styled.div`
+display: flex;
+gap: 1rem;
+`
+export const LoginOptionsMenu = () => {
+    const [isDropdownVisible, setDropdownVisible] = useState(false);
+    const dropdownRef = useRef(null);
+  
+    const toggleDropdown = () => {
+      setDropdownVisible(!isDropdownVisible);
+    };
+  
+    const closeDropdown = (e) => {
+      const dropdown = dropdownRef.current;
+  
+      // If the clicked element is outside the dropdown (button or content), close the dropdown
+      if (dropdown && !dropdown.contains(e.target)) {
+        setDropdownVisible(false);
+      }
+    };
+  
+    useEffect(() => {
+      document.addEventListener('click', closeDropdown);
+      return () => {
+        document.removeEventListener('click', closeDropdown);
+      };
+    }, []);
+  
+    return (
+      <div className="dropdown" ref={dropdownRef}>
         <LoginOptionsBtn className="dropbtn" onClick={toggleDropdown}>
           Login Options
         </LoginOptionsBtn>
         <DropdownContentContainer
           className="dropdown-content"
           style={{ display: isDropdownVisible ? 'flex' : 'none' }}
+          onMouseOver={() => setDropdownVisible(true)}
+          // kind of annoying when it closes too quickly
+        //   onMouseOut={() => setDropdownVisible(false)}
         >
-          <StyledLink >Log out</StyledLink>
-          <StyledLink >View profile</StyledLink>
+          <ItemContainer><EyeIcon />          <StyledLink>Log out</StyledLink>  </ItemContainer>
+
+          <ItemContainer><EyeIcon />          <StyledLink>View Profile</StyledLink>  </ItemContainer>
+
         </DropdownContentContainer>
       </div>
-
-      {isDropdownVisible && (
-        <div
-          onClick={closeDropdown}
-        />
-      )}
-    </div>
-  );
-};
-
+    );
+  };
+  
